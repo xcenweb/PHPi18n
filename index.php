6bd85beba6b1c90f0d08c37593588b457d5fc839
一只咕鸽 <xcenweb@qq.com>
2021-08-10 15:46:20 +0800

    补充了些内容

--- index.php
+++ index.php
@@ -8,14 +8,17 @@
  * @QQ: 1610943675
  * @time: 2021-08-10
  * @Github: github.com/xcenweb/PHPi18n
+ * @version: 1.0
  *
  */
 define('ROOT_DIR',__DIR__ . '/');//程序目录
 define('LANG_PATH',ROOT_DIR . 'i18n/');//i18n语言文件
 
+//i18n设置
 include ROOT_DIR  . 'set.php';
+//主类
 include LANG_PATH . 'i18n.class.php';
 
-//输出
+//输出语言，可以试试改系统或浏览器语言测试
 echo L('hello_world');
 ?>
